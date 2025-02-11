abd0c83ce2724fec71f2c9ca02457330af2ad0b0
TTTranslte <1776808355@qq.com>
2025-02-11 13:13:46 +0800

    pull test

--- src/main/java/com/trans/GitTest.java
+++ src/main/java/com/trans/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("snapshop");
         System.out.println("push test");
+        System.out.pringln("pull test");
     }
 }
